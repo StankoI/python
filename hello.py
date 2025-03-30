d3d35a4d9b8ba9d50ade542ae1eb831ae935484b
Stanislav <stanislavigov4@gmail.com>
2025-03-30 20:50:41 +0300

    basics

--- hello.py
+++ hello.py
@@ -5,3 +5,45 @@ a = 5
 b = 6
 
 print(a+b)
+
+str = "message"
+
+print(str)
+
+if 5 > 4:
+    print("five is greater than four")
+
+for i in range(6):
+    print(i)
+
+def myFunc():
+    print(str)
+
+myFunc()
+
+str1 = 'hello world   a'
+
+print(str1[2:7])
+
+print(str1.upper())
+print(str1.strip())
+
+age = 21
+
+txt = f"I am {21} years old"
+
+print(txt)
+
+print(txt.encode())
+
+toBeRevList = [1,2,3,4]
+
+revList = toBeRevList[::-1]
+
+print(revList)
+
+fruits = ['orange', 'kiwi', 'banana', 'pinapple']
+
+newList = [x for x in fruits if 'a' in x]
+
+print(newList)
